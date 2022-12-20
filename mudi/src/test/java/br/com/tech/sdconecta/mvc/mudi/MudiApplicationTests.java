45948ab25223a1a258714cc2e5ca76d400670789
EricklisCruz <erickliscruz@gmail.com>
2022-12-20 09:28:14 -0300

    "Spring dependencies"

---
+++ mudi/src/test/java/br/com/tech/sdconecta/mvc/mudi/MudiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tech.sdconecta.mvc.mudi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
