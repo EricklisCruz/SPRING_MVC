7b5f99ad3070562ade4950cdb519e50dddabb7c6
EricklisCruz <erickliscruz@gmail.com>
2022-12-21 16:56:08 -0300

    PEDIDO_CONTROLLER CLASS

---
+++ mudi/src/main/java/br/com/tech/sdconecta/mvc/mudi/controller/PedidoController.java
@@ -0,0 +1,35 @@
+package br.com.tech.sdconecta.mvc.mudi.controller;
+
+import br.com.tech.sdconecta.mvc.mudi.dto.RequisicaoPedido;
+import br.com.tech.sdconecta.mvc.mudi.model.Pedido;
+import br.com.tech.sdconecta.mvc.mudi.repository.PedidoRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("pedido")
+public class PedidoController {
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    @GetMapping("formulario")
+    public String form(RequisicaoPedido requisicaoPedido){
+        return "formulario";
+    }
+
+    @PostMapping("newInforms")
+    public String newInforms(@Valid RequisicaoPedido requisicaoPedido, BindingResult result){
+        if (result.hasErrors()) {
+            return "formulario";
+        }
+
+        Pedido pedido = requisicaoPedido.toPedido();
+        pedidoRepository.save(pedido);
+        return "formulario";
+    }
+}
